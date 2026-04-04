8d6902e211d07f796aba81c99a18bb15381ab2b6
zavx0z <zavx0z@yahoo.com>
2026-04-05 02:34:29 +0300

    [test] meta - добавлен тест преобразования в sqlite db
    ### Исправления в тестах:
    - Добавлен импорт createMetaforSqliteFixture
    - Добавлен тест "преобразование в sqlite db"

--- git/meta.spec.ts
+++ git/meta.spec.ts
@@ -1,8 +1,14 @@
 import { describe, test, expect } from "bun:test"
+import { createMetaforSqliteFixture } from "create-metafor/fixture"
 import meta from "./meta"
 
 describe("meta", () => {
   test("dsl формат", () => {
     expect(meta).toMatchSnapshot()
+  })
+
+  test("преобразование в sqlite db", () => {
+    createMetaforSqliteFixture(meta)
+    expect(true).toBe(true)
   })
 })
