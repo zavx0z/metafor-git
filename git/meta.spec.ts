6205574aeb9619abaeb007b2d34f9decba994b86
zavx0z <zavx0z@yahoo.com>
2026-04-05 10:09:35 +0300

    [test] meta - добавлена инициализация БД и исправлен вызов fixture
    ### Исправления в тестах:
    - Добавлены хуки beforeAll/afterAll для управления соединением с БД
    - Исправлен вызов createMetaforSqliteFixture (удален аргумент meta, добавлен вызов relation)
    - Добавлен импорт relation и типа Database

--- git/meta.spec.ts
+++ git/meta.spec.ts
@@ -1,14 +1,20 @@
-import { describe, test, expect } from "bun:test"
+import { describe, test, expect, afterAll, beforeAll } from "bun:test"
 import { createMetaforSqliteFixture } from "create-metafor/fixture"
 import meta from "./meta"
+import { relation } from "../../../pkg/sqlite"
+import type { Database } from "bun:sqlite"
 
 describe("meta", () => {
+  let db: Database
+  beforeAll(() => (db = createMetaforSqliteFixture()))
+  afterAll(() => db.close())
+
   test("dsl формат", () => {
     expect(meta).toMatchSnapshot()
   })
 
   test("преобразование в sqlite db", () => {
-    createMetaforSqliteFixture(meta)
+    relation(db, meta)
     expect(true).toBe(true)
   })
 })
